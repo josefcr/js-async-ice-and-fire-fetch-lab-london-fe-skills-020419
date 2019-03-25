e78bb5685339132338ce19f5d180f8e41d4bcd3a
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-25 18:49:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
- fetch('https://anapioficeandfire.com/api/houses')
+ fetch('https://anapioficeandfire.com/api/character')
   .then(resp => resp.json())
   .then(json => renderBooks(json));
   
